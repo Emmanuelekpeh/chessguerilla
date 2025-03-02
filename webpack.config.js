a320dd0a4ac37787b9b10f0a1bed17cd39395670
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-03-02 04:03:32 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+
+// Check if directories exist
+const imgDirExists = fs.existsSync(path.resolve(__dirname, 'src/img'));
+const cssDirExists = fs.existsSync(path.resolve(__dirname, 'src/css'));
+
+// Prepare copy patterns
+const copyPatterns = [];
+if (imgDirExists) {
+  copyPatterns.push({ from: 'src/img', to: 'img' });
+}
+if (cssDirExists) {
+  copyPatterns.push({ from: 'src/css', to: 'css' });
+}
 
 module.exports = {
-  entry: './src/js/main.js', // Using the src directory structure
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.[contenthash].js',
@@ -32,11 +46,6 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/img', to: 'img' },
-        { from: 'src/css', to: 'css' }
-      ]
-    })
+    ...(copyPatterns.length > 0 ? [new CopyWebpackPlugin({ patterns: copyPatterns })] : [])
   ]
 };
