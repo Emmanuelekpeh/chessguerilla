3755471840a14e62842a29d8613f6c26d9a6c574
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-03-02 19:41:08 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const fs = require('fs');
-
-// Build copy patterns based on existing directories
-const copyPatterns = [];
-
-// Check if directories exist before adding them to copy patterns
-if (fs.existsSync(path.resolve(__dirname, 'src/css'))) {
-  copyPatterns.push({ from: 'src/css', to: 'css' });
-}
-
-if (fs.existsSync(path.resolve(__dirname, 'src/img'))) {
-  copyPatterns.push({ from: 'src/img', to: 'img' });
-}
 
 module.exports = {
   entry: './src/js/main.js',
@@ -40,18 +27,20 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    extensions: ['.js']
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
       inject: 'body'
     }),
-    ...(copyPatterns.length > 0 ? [new CopyWebpackPlugin({ patterns: copyPatterns })] : [])
-  ],
-  externals: {
-    // Mark these as external since they're loaded via CDN
-    'jquery': 'jQuery',
-    'chess.js': 'Chess',
-    '@chrisoakman/chessboardjs': 'Chessboard'
-  }
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/css', to: 'css' },
+        { from: 'src/img', to: 'img' }
+      ]
+    })
+  ]
 };
